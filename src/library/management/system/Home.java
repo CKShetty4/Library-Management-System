c1dc587e32af8fe377b0446de36ca226dc2649a8
CKShetty <dev.ckshetty@gmail.com>
2025-01-05 10:12:51 +0000

    add: issue book

--- src/library/management/system/Home.java
+++ src/library/management/system/Home.java
@@ -116,6 +116,8 @@ public class Home extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+
+         new IssueBook().setVisible(true);
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton4ActionPerformed
 
